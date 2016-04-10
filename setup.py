ea089542d0aec91edb7c769e52e4d22a50ffa21e
wrwrwr <git@wr.waw.pl>
2016-04-10 21:29:27 +0200

    Configured Tox test automation for Python 3.4 and 3.5.

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup
 
 setup(
     name='scikit-gof',
-    version='0.1.1',
+    version='0.1.2',
     packages=('skgof',),
     install_requires=('numpy>=1.10', 'scipy'),
     tests_require=('pytest', 'pytest-benchmark', 'pytest-flake8',
                    'pytest-isort', 'pytest-readme',
                    'flake8-print', 'flake8-todo', 'pep8-naming'),
-
     description="Variations on goodness of fit tests for SciPy.",
     long_description=open('README.rst').read(),
     author="Wojciech Ruszczewski",
@@ -23,5 +22,6 @@ setup(
         "Topic :: Scientific/Engineering :: Information Analysis",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ),
 )
