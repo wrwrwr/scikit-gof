ad949b8d8e9be2022827a391035ff1e2c0194196
wrwrwr <git@wr.waw.pl>
2017-02-15 22:58:21 +0100

    Made setup() parameters available for tools.

--- setup.py
+++ setup.py
@@ -2,26 +2,39 @@
 
 from setuptools import setup
 
-setup(
-    name='scikit-gof',
-    version='0.1.2',
-    packages=('skgof',),
-    install_requires=('numpy>=1.10', 'scipy'),
-    tests_require=('pytest', 'pytest-benchmark', 'pytest-flake8',
-                   'pytest-isort', 'pytest-readme',
-                   'flake8-print', 'flake8-todo', 'pep8-naming'),
-    description="Variations on goodness of fit tests for SciPy.",
-    long_description=open('README.rst').read(),
-    author="Wojciech Ruszczewski",
-    author_email="scipy@wr.waw.pl",
-    url="http://github.org/wrwrwr/scikit-gof",
-    license="MIT",
-    classifiers=(
+meta = {
+    'name': 'scikit-gof',
+    'version': '0.1.2',
+    'packages': ('skgof',),
+    'install_requires': (
+        'numpy>=1.10',
+        'scipy>=0.16'
+    ),
+    'tests_require': (
+        'flake8-print',
+        'flake8-todo',
+        'pep8-naming',
+        'pytest',
+        'pytest-benchmark',
+        'pytest-flake8',
+        'pytest-isort',
+        'pytest-readme'
+    ),
+    'description': "Variations on goodness of fit tests for SciPy.",
+    'long_description': open('README.rst').read(),
+    'author': "Wojciech Ruszczewski",
+    'author_email': "scipy@wr.waw.pl",
+    'url': "http://github.org/wrwrwr/scikit-gof",
+    'license': "MIT",
+    'classifiers': (
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ),
-)
+        "Programming Language :: Python :: 3.5"
+    )
+}
+
+if __name__ == '__main__':
+    setup(**meta)
